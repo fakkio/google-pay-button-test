2e90ff2a102cd73165a1f7c32ea58f2ba2273dd7
Fabio <lazzaroni@jeflab.it>
2022-11-14 15:20:10 +0100

    💄: Favicon e titolo

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "Google Pay button test",
   viewport: "width=device-width,initial-scale=1",
 });
 
